8457b0f3e3ec9ecfc8e2a385029a5a0abe2ade7e
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-03-07 19:07:45 +0300

    * register and login schemas * user schema * obtain and refresh token schema * message schema

---
+++ schemas.py
@@ -0,0 +1,62 @@
+from marshmallow import Schema, fields, validate
+
+
+class RegisterUser(Schema):
+    username = fields.String(
+        required=True,
+        validate=validate.Length(max=100)
+    )
+    password = fields.String(
+        required=True,
+        validate=validate.Length(max=100)
+    )
+    password2 = fields.String(
+        required=True,
+        validate=validate.Length(max=100)
+    )
+    email = fields.String(
+        required=True,
+        validate=validate.Email
+    )
+
+    class Meta:
+        strict = True
+
+
+class LoginUser(Schema):
+    username = fields.String(
+        required=True,
+        validate=validate.Length(max=100)
+    )
+    password = fields.String(
+        required=True,
+        validate=validate.Length(max=100)
+    )
+
+    class Meta:
+        strict = True
+
+
+class User(Schema):
+    id = fields.Integer(dump_only=True)
+    username = fields.String(dump_only=True)
+    email = fields.String(dump_only=True)
+    is_active = fields.Boolean(dump_only=True)
+    created = fields.DateTime(dump_only=True)
+    last_login = fields.DateTime(dump_only=True)
+
+
+class Token(Schema):
+    access_token = fields.String(dump_only=True)
+    refresh_token = fields.String(dump_only=True)
+
+
+class RefreshToken(Schema):
+    refresh_token = fields.String(required=True)
+
+
+class Message(Schema):
+    message = fields.String(
+        dump_only=True,
+        required=True
+    )
