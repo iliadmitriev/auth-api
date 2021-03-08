99f5f80e159bc7027715ba457b17ebc3454006cf
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-03-08 17:15:07 +0300

    * generate access and refresh token function * get data from request function

--- utils.py
+++ utils.py
@@ -1,9 +1,17 @@
 import hashlib
+from datetime import datetime, timedelta
+import jwt
 from base64 import b64encode
-from settings import SECRET_KEY
+from settings import (
+    SECRET_KEY,
+    JWT_ALGORITHM,
+    JWT_EXP_ACCESS_SECONDS,
+    JWT_EXP_REFRESH_SECONDS
+)
+from uuid import uuid4
 
 
-def generate_password_hash(passwd: str) -> str:
+async def generate_password_hash(passwd: str) -> str:
     dk = hashlib.pbkdf2_hmac(
         'sha256',
         passwd.encode('utf-8'),
@@ -12,3 +20,33 @@ def generate_password_hash(passwd: str) -> str:
     )
     return b64encode(dk).decode('ascii').strip()
 
+
+async def get_data_from_request(request):
+    if request.content_type == 'application/json':
+        data = await request.json()
+    else:
+        data = await request.post()
+    return data
+
+
+async def gen_token_for_user(user):
+    token = {
+        'user_id': user.get('id'),
+        'email': user.get('email'),
+        'jti': uuid4().hex,
+    }
+
+    access_token = token | {
+        'token_type': 'access_token',
+        'exp': datetime.utcnow() + timedelta(seconds=JWT_EXP_ACCESS_SECONDS)
+    }
+
+    refresh_token = token | {
+        'token_type': 'refresh_token',
+        'exp': datetime.utcnow() + timedelta(seconds=JWT_EXP_REFRESH_SECONDS)
+    }
+
+    return {
+        "access_token": jwt.encode(access_token, SECRET_KEY, JWT_ALGORITHM),
+        "refresh_token": jwt.encode(refresh_token, SECRET_KEY, JWT_ALGORITHM)
+    }
